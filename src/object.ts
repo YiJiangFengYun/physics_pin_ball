a89bbf0d472b35e7a49bd301b8edc94c857136a9
YiJiangFengYun <yijiangfengyun@outlook.com>
2018-08-16 17:51:50 +0800

    Object add member velocity.

--- src/object.ts
+++ src/object.ts
@@ -4,9 +4,11 @@ import { Bounds } from "./bounds";
 export abstract class Obj {
     protected _pos:Vector;
     protected _bounds:Bounds;
+    protected _velocity:Vector;
     constructor() {
         this._pos = new Vector();
         this._bounds = new Bounds();
+        this._velocity = new Vector();
     }
 
     get pos():Vector {
@@ -19,5 +21,13 @@ export abstract class Obj {
 
     get bounds():Bounds {
         return this._bounds;
+    }
+
+    get velocity():Vector {
+        return this._velocity;
+    }
+
+    set velocity(value:Vector) {
+        this._velocity = value;
     }
 }
