178e6d9c3cdabb43afb1af900de25a6ba7eba69e
YiJiangFengYun <yijiangfengyun@outlook.com>
2018-08-16 16:43:55 +0800

    Add class Bounds.

---
+++ src/bounds.ts
@@ -0,0 +1,25 @@
+export class Bounds {
+    public static intersectBounds(bounds1:Bounds, bounds2:Bounds):boolean {
+        if (bounds1.minX > bounds2.maxX || bounds1.maxX < bounds2.minX || 
+            bounds1.minY > bounds2.maxY || bounds1.maxY < bounds2.minY) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    minX:number;
+    minY:number;
+    maxX:number;
+    maxY:number;
+    constructor(minX:number = 0, minY:number = 0, maxX:number = 0, maxY:number = 0) {
+        this.minX = minX;
+        this.minY = minY;
+        this.maxX = maxX;
+        this.maxY = maxY;
+    }
+
+    intersect(target:Bounds):boolean {
+        return Bounds.intersectBounds(this, target);
+    }
+}
