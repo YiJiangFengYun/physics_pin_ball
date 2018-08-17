e59a64f88f34394f0b323c376ca2641198fdf972
YiJiangFengYun <yijiangfengyun@outlook.com>
2018-08-17 17:08:17 +0800

    Change my_circle.

--- src/my_circle.ts
+++ src/my_circle.ts
@@ -15,17 +15,40 @@ export class MyCircle extends Circle {
                 normal: null,
             };
         if (target instanceof Circle) {
-            let my_circle = this;
-            let target_circle = target as Circle;
-            let normal = Vector.subVectors(my_circle.pos, target_circle.pos);
-            let distance = normal.magnitude();
-            if (distance < (my_circle.radius + target_circle.radius)) {
-                result.collided = true;
-                normal.normal();
-                result.normal = normal;
+            let myCircle = this;
+            let targetCircle = target as Circle;
+            if (myCircle.bounds.intersect(targetCircle.bounds)) {
+                let normal = Vector.subVectors(myCircle.pos, targetCircle.pos);
+                let distance = normal.magnitude();
+                if (distance < (myCircle.radius + targetCircle.radius)) {
+                    result.collided = true;
+                    normal.normal();
+                    result.normal = normal;
+                }
             }
         } else if (target instanceof Square) {
+            let myCircle = this;
+            let targetSquare = target as Square;
+            if (myCircle.bounds.intersect(targetSquare.bounds)) {
+                let circleCenter = myCircle.pos;
+                let circleCenterX = circleCenter.x;
+                let circleCenterY = circleCenter.y;
+                let squareBounds = targetSquare.bounds;
+                if (circleCenterX > squareBounds.minX && circleCenterX < squareBounds.maxX) {
+                    if (circleCenterY > squareBounds.minY && circleCenterY < squareBounds.maxY) {
+                        //The center is inside the bounds (namely, inside the square).
+                        result.collided = true;
+                        result.normal = null;
+                    } else if (circleCenterY > squareBounds.maxY) {
+                        //The center is upside the bounds.
 
+                    } else if (circleCenterY < squareBounds.minY) {
+                        
+                    }
+                } else if (circleCenterY > squareBounds.minY && circleCenterY < squareBounds.maxY) {
+
+                }
+            }
         } else {
             throw new Error("Invalid shape for collision detection.");
         }
