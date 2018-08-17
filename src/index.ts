d3b7b9fcdf09e43aa9e2f5aa4b6c1b1d9419f4c6
YiJiangFengYun <yijiangfengyun@outlook.com>
2018-08-17 16:32:44 +0800

    Export libaray classes in index.ts.

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { unitSize } from "./unit";
+export { Vector } from "./vector";
+export { Bounds } from "./bounds";
+export { Obj } from "./object";
+export { Circle } from "./circle";
+export { Square } from "./square";
+export { World } from "./world";
+export { MyCircle } from "./my_circle";
