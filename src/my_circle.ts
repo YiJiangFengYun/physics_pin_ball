4cd3bf83bdb0cc1c14e1842f8001cafd48df7e20
YiJiangFengYun <yijiangfengyun@outlook.com>
2018-08-20 09:41:25 +0800

    MyCircle add remain collide detect.

--- src/my_circle.ts
+++ src/my_circle.ts
@@ -33,6 +33,7 @@ export class MyCircle extends Circle {
                 let circleCenter = myCircle.pos;
                 let circleCenterX = circleCenter.x;
                 let circleCenterY = circleCenter.y;
+                let circleRadius = myCircle.radius;
                 let squareBounds = targetSquare.bounds;
                 if (circleCenterX > squareBounds.minX && circleCenterX < squareBounds.maxX) {
                     if (circleCenterY > squareBounds.minY && circleCenterY < squareBounds.maxY) {
@@ -40,13 +41,48 @@ export class MyCircle extends Circle {
                         result.collided = true;
                         result.normal = null;
                     } else if (circleCenterY > squareBounds.maxY) {
-                        //The center is upside the bounds.
+                        //The center is downside the bounds.
+                        if (circleCenterY - squareBounds.maxY < circleRadius) {
+                            result.collided = true;
+                            result.normal = new Vector(0, 1);
+                        }
 
                     } else if (circleCenterY < squareBounds.minY) {
-                        
+                        //The center is upside the bounds.
+                        if (squareBounds.minY - circleCenterY < circleRadius) {
+                            result.collided = true;
+                            result.normal = new Vector(0, -1);
+                        }
                     }
                 } else if (circleCenterY > squareBounds.minY && circleCenterY < squareBounds.maxY) {
-
+                    if (circleCenterX > squareBounds.maxX) {
+                        //The center is right of the bounds.
+                        if (circleCenterX - squareBounds.maxX < circleRadius) {
+                            result.collided = true;
+                            result.normal = new Vector(1, 0);
+                        }
+                    } else if (circleCenterX < squareBounds.minX) {
+                        //The center is left of the bounds.
+                        if (squareBounds.minX - circleCenterX < circleRadius) {
+                            result.collided = true;
+                            result.normal = new Vector(-1, 0);
+                        }
+                    }
+                } else {
+                    //Detect if any point of square is inside the circle.
+                    let points = targetSquare.points;
+                    let pointCount = points.length;
+                    let pointHelper:Vector = new Vector();
+                    let circleRadiusSquare:number = circleRadius * circleRadius;
+                    for (let i = 0; i < pointCount; ++i) {
+                        Vector.subVectors(circleCenter, points[i], pointHelper);
+                        if (pointHelper.manitudeSquare() < circleRadiusSquare) {
+                            //The point is inside the circle.
+                            result.collided = true;
+                            result.normal = pointHelper;
+                            break;
+                        }
+                    }
                 }
             }
         } else {
