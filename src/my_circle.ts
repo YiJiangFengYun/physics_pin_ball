244d0adcf0f4eca90876221fe283b66a6409436d
YiJiangFengYun <yijiangfengyun@outlook.com>
2018-08-21 16:12:14 +0800

    Add IRTriangle.

--- src/my_circle.ts
+++ src/my_circle.ts
@@ -2,6 +2,7 @@ import { Circle } from "./circle";
 import { Obj } from "./object";
 import { Rectangle } from "./rectangle";
 import { Vector } from "./vector";
+import { IRTriangle } from "./triangle";
 
 export interface ICollideResult {
     collided: boolean; normal:Vector;
@@ -104,6 +105,89 @@ export class MyCircle extends Circle {
                     }
                 }
             }
+        } else if (target instanceof IRTriangle) {
+            let myCircle = this;
+            let targetTriangle = target as IRTriangle;
+            if (myCircle.bounds.intersect(targetTriangle.bounds)) {
+                let circleCenter = myCircle.pos;
+                let circleCenterPos = [circleCenter.x, circleCenter.y];
+                let circleRadius = myCircle.radius;
+                let triangleBounds = targetTriangle.bounds;
+                let triangleBoundsMin = [triangleBounds.minX, triangleBounds.minY];
+                let triangleBoundsMax = [triangleBounds.maxX, triangleBounds.maxY];
+                //00 is left 01 is midle 10 is right
+                //First two bit is x and second two bit is y.
+                let circlArea = 0;
+                if (circleCenterPos[0] > triangleBoundsMin[0] && circleCenterPos[0] < triangleBoundsMax[0]) {
+                    circlArea |= 1;
+                } else if (circleCenterPos[0] > triangleBoundsMax[0]) {
+                    circlArea |= 2;
+                }
+                if (circleCenterPos[1] > triangleBoundsMin[1] && circleCenterPos[1] < triangleBoundsMax[1]) {
+                    circlArea |= (1 << 2);
+                } else if (circleCenterPos[1] > triangleBoundsMax[1]) {
+                    circlArea |= (2 << 2);
+                }
+                //rectangle 9 area.
+                let findArea = false;
+                for (let areaX = 0; areaX < 3; ++areaX) {
+                    for (let areaY = 0; areaY < 3; ++areaY) {
+                        if ((circlArea ^ areaX) == 0 && ((circlArea >> 2) ^ areaY) == 0) {
+                            findArea = true;
+                            let sameXDirect = areaX != 1 && (areaX >> 1) == (targetTriangle.direct & 1);
+                            let sameYDirect = areaY != 1 && (areaY >> 1) == ((targetTriangle.direct >> 1) & 1);
+                            if (sameXDirect && sameYDirect) {
+                                if (areaX != 1 && areaY != 1) {
+                                    //The circle is corner of the square.
+                                    //Detect if any point of square is inside the circle.
+                                    let points = targetTriangle.points;
+                                    let point = points[((areaY >> 1) << 1) | (areaX >> 1)];
+                                    let pointHelper = new Vector();
+                                    let circleRadiusSquare:number = circleRadius * circleRadius;
+                                    Vector.subVectors(circleCenter, point, pointHelper);
+                                    if (pointHelper.manitudeSquare() < circleRadiusSquare) {
+                                        //The point is inside the circle.
+                                        result.collided = true;
+                                        Vector.normalVector(pointHelper, result.normal);
+                                    }  
+                                } else {
+                                    let points = targetTriangle.points;
+                                    //Right angle point.
+                                    let point = points[targetTriangle.direct];
+                                    if (areaX != 1) {
+                                        if (Math.abs(point.x - circleCenterPos[0]) < circleRadius) {
+                                            result.collided = true;
+                                            result.normal = new Vector(areaX - 1, 0);
+                                        }
+                                    } else {
+                                        if (Math.abs(point.y - circleCenterPos[1]) < circleRadius) {
+                                            result.collided = true;
+                                            result.normal = new Vector(0, areaY);
+                                        }
+                                    }
+                                }
+                            } else {
+                                let vectorTriangleToCenter = Vector.subVectors(myCircle.pos, targetTriangle.pos);
+                                let points = targetTriangle.points;
+                                    //Right angle point.
+                                let point = points[targetTriangle.direct];
+                                let normal = Vector.subVectors(targetTriangle.pos, point);
+                                normal.normal();
+                                let dot = Vector.dotVectors(vectorTriangleToCenter, normal);
+                                let circleRadius = myCircle.radius;
+                                if (dot < circleRadius) {
+                                    result.collided = true;
+                                    result.normal = normal;
+                                }
+                            }
+                            break;
+                        }
+                    }
+                    if (findArea) break;
+                }
+            }
+            
+
         } else {
             throw new Error("Invalid shape for collision detection.");
         }
---
+++ src/triangle.ts
@@ -0,0 +1,91 @@
+import { Obj } from "./object";
+import { Vector } from "./vector";
+import { Rectangle } from "./rectangle";
+
+//Right angle direct.
+export enum DirectIRTriangle {
+    LEFT_UP = 0,
+    RIGHT_UP = 1,
+    RIGHT_BOTTOM = 3,
+    LEFT_BOTTOM = 2,
+}
+
+export class IRTriangle extends Obj {
+    public direct:DirectIRTriangle;
+    private _size:number;
+    static POINT_COUNT:number = 4;
+    private _points:Vector[];
+    constructor() {
+        super();
+        this._size = 0;
+        let points:Vector[] = [];
+        this._points = points;
+        const POINT_COUNT = Rectangle.POINT_COUNT;
+        points.length = POINT_COUNT;
+        for (let i = 0; i < POINT_COUNT; ++i) {
+            points[i] = new Vector();
+        }
+        this._setBounds();
+        this._setPoints();
+    }
+
+    get size() {
+        return this._size;
+    }
+
+    set size(value:number) {
+        this._size = value;
+        this._setBounds();
+        this._setPoints();
+    }
+
+    get pos() {
+        return this._pos;
+    }
+
+    set pos(value:Vector) {
+        var pos = this._pos;
+        pos.copy(value);
+        
+        this._setBounds();
+        this._setPoints();
+        
+    }
+
+    updatePos(value?:Vector) {
+        super.updatePos(value);
+        this._setBounds();
+        this._setPoints();
+    }
+
+    get points() {
+        return this._points;
+    }
+
+    _setBounds() {
+        let pos = this._pos;
+        let bounds = this._bounds;
+        let halfSize = this._size / 2;
+        bounds.minX = pos.x - halfSize;
+        bounds.minY = pos.y - halfSize;
+        bounds.maxX = pos.x + halfSize;
+        bounds.maxY = pos.y + halfSize;
+    }
+
+    _setPoints() {
+        let bounds = this._bounds;
+        let points = this._points;
+        //0 is 0(y)0(x)
+        points[0].x = bounds.minX;
+        points[0].y = bounds.minY;
+        //1 is 0(y)1(x)
+        points[1].x = bounds.maxX;
+        points[1].y = bounds.minY;
+        //3 is 1(y)1(x)
+        points[3].x = bounds.maxX;
+        points[3].y = bounds.maxY;
+        //2 is 1(y)0(x)
+        points[2].x = bounds.minX;
+        points[2].y = bounds.maxY;
+    }
+}
